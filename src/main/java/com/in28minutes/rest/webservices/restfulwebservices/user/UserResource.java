b8242ce843152ca0602dce135926d79f9ad9a28b
dwelguisz <dwelguisz@indeed.com>
2021-10-25 21:43:39 -0500

    Completed Step 10.  POST User Body

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,12 +1,15 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -34,7 +37,13 @@ public class UserResource {
     // input - details of user
     // output - CREATED & Return the created URI
     @PostMapping(path = "/users")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
         User savedUser = userDaoService.save(user);
+        //CREATED
+        // /users/{id}   - savedUser.getId()
+        URI newResource = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(newResource).build();
     }
 }
