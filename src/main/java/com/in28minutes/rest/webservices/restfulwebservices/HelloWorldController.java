fa956967e87caac1d5ae4608348c8d19c8d3838f
dwelguisz <dwelguisz@indeed.com>
2021-10-25 20:53:22 -0500

    Step 06 work

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 //Controller
@@ -20,5 +21,12 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
     }
+
+    //hello-world-bean
+    @GetMapping(path="/hello-world/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello world, %s", name));
+    }
+
 
 }
