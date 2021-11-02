2b43d133c81201dc23716ee273cbecff930a50e2
dwelguisz <dwelguisz@indeed.com>
2021-11-02 15:33:44 -0500

    STEP19: Added XML and messages for Spanish and Dutch

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,5 +1,8 @@
 package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 //Controller
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     //GET
     //URI = /hello-world
@@ -26,6 +32,14 @@ public class HelloWorldController {
     @GetMapping(path="/hello-world/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello world, %s", name));
+    }
+
+    @GetMapping(path="/hello-world-i18n")
+    public String helloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, "Default Message", LocaleContextHolder.getLocale());
+        //en = "Hello World"
+        //nl = Goede Morgen
+        //fr = Bonjour
     }
 
 
