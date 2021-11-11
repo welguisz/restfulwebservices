32b2703dd1d8b8f874bafdf7922c1bcb68412712
dwelguisz <dwelguisz@indeed.com>
2021-11-11 12:03:50 -0600

    Went from static user to dynamic user

---
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserJPAResource.java
@@ -0,0 +1,73 @@
+package com.in28minutes.rest.webservices.restfulwebservices.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.validation.Valid;
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
+
+@RestController
+public class UserJPAResource {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserJPAResource(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    //GET /users
+    @GetMapping(path="/jpa/users")
+    public List<User> retrieveAllUsers() {
+        return userRepository.findAll();
+    }
+
+    //GET /users/{id} -> /users/1
+    @GetMapping(path = "/jpa/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
+        Optional<User> user = userRepository.findById(id);
+        if (!user.isPresent()) {
+            throw new UserNotFoundException("id-"+id);
+        }
+        EntityModel<User> model = EntityModel.of(user.get());
+        WebMvcLinkBuilder linkToUsers = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        model.add(linkToUsers.withRel("all-users"));
+        return model;
+    }
+
+    // CREATED
+    // input - details of user
+    // output - CREATED & Return the created URI
+    @PostMapping(path = "/jpa/users")
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
+        User savedUser = userRepository.save(user);
+        //CREATED
+        // /users/{id}   - savedUser.getId()
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+    //GET /users/{id} -> /users/1
+    @DeleteMapping(path = "/jpa/users/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
+    }
+
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values(10001, sysdate(), 'Amy');
+insert into user values(10002, sysdate(), 'Jam');
+insert into user values(10003, sysdate(), 'Jill');
+insert into user values(10004, sysdate(), 'Chuck');
